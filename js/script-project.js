b20b6a7f635304937aa42e001806ec0f8c02d25a
moonGue <sasha.mojseenko2001@gmail.com>
2019-10-19 01:49:05 +0300

    feat: refract slider and add swiper

--- js/script-project.js
+++ js/script-project.js
@@ -2,27 +2,38 @@ document.addEventListener('DOMContentLoaded', () => {
     let btnToggle = document.createElement('button'),
         btnBack = document.createElement('a');
 
+    if (window.innerWidth > 375) {
+        document.body.appendChild(btnToggle);
+    }
     btnToggle.textContent = 'Mobile';
     btnToggle.classList.add('btn-toggle-screen');
     btnBack.textContent = 'Back';
     btnBack.classList.add('btn-back');
     btnBack.href = 'index.html';
-    document.body.appendChild(btnToggle);
+
     document.body.appendChild(btnBack);
 
     let btn = document.querySelector('.btn-toggle-screen'),
-        btn2 = document.querySelector('.btn-back');
-
-
-    btn.style.cssText = `position: fixed;bottom: 110px;right:40px; border-radius: 10px; font-size: 32px; width: 220px; height: 70px; border: none; cursor: pointer; background: #000; color: #fff;`;
-    btn2.style.cssText = `position: fixed;bottom: 20px;right:40px; border-radius: 10px; font-size: 32px; display: flex; justify-content: center; text-decoration: none; align-items: center; width: 220px; height: 70px; border: none; cursor: pointer; background: #000; color: #fff;`;
-
-    let frame = document.querySelector('.frame'),
+        btn2 = document.querySelector('.btn-back'),
+        frame = document.querySelector('.frame'),
         box = document.querySelector('.box');
 
-    btn.addEventListener('click', () => {
-        btn.textContent === 'Mobile' ? btn.textContent = 'Desktop' : btn.textContent = 'Mobile';
-        frame.classList.toggle('mobile');
-        box.classList.toggle('dark');
-    })
+    if (btn) {
+        btn.style.cssText = `position: fixed;bottom: 110px;right:40px; border-radius: 10px; font-size: 32px; width: 220px; height: 70px; border: none; cursor: pointer; background: #000; color: #fff;`;
+        btn.addEventListener('click', () => {
+            btn.textContent === 'Mobile' ? btn.textContent = 'Desktop' : btn.textContent = 'Mobile';
+            frame.classList.toggle('mobile');
+            box.classList.toggle('dark');
+        })
+    }
+    btn2.style.cssText = `position: fixed;bottom: 20px;right:40px; border-radius: 10px; font-size: 32px; display: flex; justify-content: center; text-decoration: none; align-items: center; width: 220px; height: 70px; border: none; cursor: pointer; background: #000; color: #fff;`;
+
+    if (innerWidth < 524) {
+        btn2.style.cssText = `position: fixed;bottom: 20px;right:40px; border-radius: 10px; font-size: 22px; display: flex; justify-content: center; text-decoration: none; align-items: center; width: 120px; height: 40px; border: none; cursor: pointer; background: #000; color: #fff;`;
+        if (btn) {
+            btn.style.cssText = `position: fixed;bottom: 80px;right:40px; border-radius: 10px; font-size: 22px; width: 120px; height: 40px; border: none; cursor: pointer; background: #000; color: #fff;`;
+        }
+    }
+
+
 });
--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let btnShow = document.querySelectorAll('.btn-show'),
-        slides = document.querySelectorAll('.slide'),
-        next = document.querySelector('.arrow__direction_next'),
-        prev = document.querySelector('.arrow__direction_prev'),
-        slideIndex = 1;
+    let btnShow = document.querySelectorAll('.btn-show');
 
-    // Dropdown description
+    // Dropdown description slide
     btnShow.forEach((item) => {
         item.addEventListener('click', (e) => {
             let target = e.target,
@@ -16,25 +12,133 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     //Slider
-    let showSlides = (n) => {
-        if (n > slides.length) slideIndex = 1;
-        if (n < 1) slideIndex = slides.length;
+    let slides = document.querySelectorAll('.slide'),
+        currentSlide = 0,
+        btnNext = document.querySelector('.arrow__direction_next'),
+        btnPrev = document.querySelector('.arrow__direction_prev'),
+        isEnable = true;
 
-        slides.forEach((item) => item.style.display = 'none');
-        slides[slideIndex - 1].style.display = 'flex';
-    };
-    showSlides(slideIndex);
+    function changeCurrentSlide(n) {
+        currentSlide = (n + slides.length) % slides.length;
+    }
 
-    let plusSlides = (n) => {
-        showSlides(slideIndex += n);
-    };
+    function hideSlide(direction) {
+        isEnable = false;
+        slides[currentSlide].classList.add(direction);
+        slides[currentSlide].addEventListener('animationend', function () {
+            this.classList.remove('slide_active', direction);
+        })
+    }
 
-    prev.addEventListener('click', () => {
-        plusSlides(-1);
+    function showSlide(direction) {
+        slides[currentSlide].classList.add('slide_next', direction);
+        slides[currentSlide].addEventListener('animationend', function () {
+            this.classList.remove('slide_next', direction);
+            this.classList.add('slide_active');
+            isEnable = true;
+        })
+    }
+
+    function nextSlide(n) {
+        hideSlide('to-left');
+        changeCurrentSlide(n + 1);
+        showSlide('from-right');
+    }
+
+    function prevSlide(n) {
+        hideSlide('to-right');
+        changeCurrentSlide(n - 1);
+        showSlide('from-left');
+    }
+
+    btnNext.addEventListener('click', () => {
+        if (isEnable) nextSlide(currentSlide);
     });
 
-    next.addEventListener('click', () => {
-        plusSlides(1);
+    btnPrev.addEventListener('click', () => {
+        if (isEnable) prevSlide(currentSlide);
+    });
+
+    //Swiper
+    function swipeDetect(el) {
+        let surface = el,
+            startX = 0,
+            startY = 0,
+            distX = 0,
+            distY = 0,
+            startTime = 0,
+            elapsedTime = 0,
+            threshold = 150,
+            restraint = 100,
+            allowedTime = 300;
+
+        surface.addEventListener('mousedown', (e) => {
+            startX = e.pageX;
+            startY = e.pageY;
+            startTime = new Date().getTime;
+            e.preventDefault();
+        }, false);
+
+        surface.addEventListener('mouseup', (e) => {
+            distX = e.pageX - startX;
+            distY = e.pageY - startY;
+            elapsedTime = startTime - new Date().getTime();
+
+            if (allowedTime <= allowedTime) {
+                if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+                    if (distX > 0 && isEnable) prevSlide(currentSlide);
+                    else if (isEnable) nextSlide(currentSlide);
+                }
+            }
+        });
+
+        surface.addEventListener('touchstart', (e) => {
+            let target = e.target;
+            if (target.classList.contains('arrow__direction') || target.classList.contains('arrow')) {
+                if (target.classList.contains('arrow__direction_prev') && isEnable) prevSlide(currentSlide);
+                else if (isEnable) nextSlide(currentSlide);
+            }
+
+            let touchItem = e.changedTouches[0];
+            startX = touchItem.pageX;
+            startY = touchItem.pageY;
+            startTime = new Date().getTime();
+            console.log(startX, startY, startTime)
+        }, false);
+
+
+        surface.addEventListener('touchend', (e) => {
+            let touchItem = e.changedTouches[0];
+            distX = touchItem.pageX - startX;
+            distY = touchItem.pageY - startY;
+            elapsedTime = new Date().getTime() - startTime;
+
+            console.log(distX, startY, elapsedTime);
+            if (elapsedTime <= allowedTime) {
+                if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+                    if (distX > 0 && isEnable) prevSlide(currentSlide);
+                    else if (isEnable) nextSlide(currentSlide);
+                }
+            }
+        }, false);
+    }
+
+    let el = document.querySelector('.slider');
+    swipeDetect(el);
+
+
+    // Dropdown description swiper
+    let btnDescrSwipe = document.querySelectorAll('.btn-swipe-description');
+
+    btnDescrSwipe.forEach((item) => {
+        item.addEventListener('click', (e) => {
+            let target = e.target,
+                swipeList = target.closest('.slide').querySelector('.slide__description');
+
+            target.classList.toggle('active');
+            swipeList.classList.toggle('slide__description_show');
+        });
     })
 
 });
+
